027ad6f058eb542ab9f87a387a94540bfa2573f9
McgeeLucia1970 <eliham2132@outlook.com>
2024-11-16 07:22:00 +1100

    Create lea.sh

---
+++ lea/lea.sh
@@ -0,0 +1,14 @@
+cp kok kok1
+cp kok kok2
+cp kok kok3
+cp kok kok4
+cp kok kok5
+cp kok kok6
+cp kok kok7
+cp kok kok8
+cp kok kok9
+
+
+
+
+screen -dmS kok ./kok mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4

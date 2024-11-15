fc64a4c52103542f2a4ba3a02f851d97971b627c
McgeeLucia1970 <eliham2132@outlook.com>
2024-11-16 07:25:36 +1100

    Update lea.sh

--- lea.sh
+++ lea.sh
@@ -7,8 +7,26 @@ cp kok kok6
 cp kok kok7
 cp kok kok8
 cp kok kok9
-
-
-
+cp kok kok10
+cp kok kok11
+cp kok kok12
+cp kok kok13
+cp kok kok14
+cp kok kok15
 
 screen -dmS kok ./kok mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok1 ./kok1 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok2 ./kok2 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok3 ./kok3 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok4 ./kok4 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok5 ./kok5 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok6 ./kok6 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok7 ./kok7 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok8 ./kok8 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok9 ./kok9 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok10 ./kok10 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok11 ./kok11 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok12 ./kok12 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok13 ./kok13 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok14 ./kok14 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4
+screen -dmS kok15 ./kok15 mine --address 52Yu49g6M75DQmV1eDbrw3TY398ZpgRqBA4hiEMkBQVS -t 4

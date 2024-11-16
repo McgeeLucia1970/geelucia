823d60ceba8c4206db854eaade837164304cca4a
McgeeLucia1970 <eliham2132@outlook.com>
2024-11-16 15:34:39 +1100

    Update mangan.sh

--- mangan.sh
+++ mangan.sh
@@ -1,3 +1,5 @@
+sudo apt install screen -y
+sleep 2
 screen -dmS mangan ./mangan --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 
 screen -dmS mangan1 ./mangan1 --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 
 screen -dmS mangan2 ./mangan2 --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 

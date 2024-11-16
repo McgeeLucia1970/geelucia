c7098b1b81e0da2fca946d5c0545ea29ced4a344
McgeeLucia1970 <eliham2132@outlook.com>
2024-11-16 15:27:08 +1100

    Update mangan.sh

--- mangan.sh
+++ mangan.sh
@@ -1,3 +1,5 @@
-#!/usr/bin/bash
-
-./mangan --keypair /content/geelucia/id.json protomine --threads 96 >/dev/null 2>&1 
+screen -dmS mangan ./mangan --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 
+screen -dmS mangan1 ./mangan1 --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 
+screen -dmS mangan2 ./mangan2 --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 
+screen -dmS mangan3 ./mangan3 --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 
+screen -dmS mangan4 ./mangan4 --keypair /content/geelucia/id.json protomine --threads 20 >/dev/null 2>&1 
